9c0ccda60b5043b610ada481119f59ece4e67fa5
Eduardo Carvalho <carlos.eduardo.carvalho@ee.ufcg.edu.br>
2026-09-12 19:03:36 -0300

    refactor

--- android/app/src/main/java/com/androsid/StreamServer.kt
+++ android/app/src/main/java/com/androsid/StreamServer.kt
@@ -68,8 +68,8 @@ class StreamServer(
                     if (running) Log.e(TAG, "accept loop died", e)
                 }
             }
-        } 
-        
+        }
+
         thread(name = "androsid-watchdog", isDaemon = true) {
             while (running) {
                 Thread.sleep(idleTimeoutMs)
