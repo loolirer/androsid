08fb3e6a7349196052ec49cb31110a5b5e718074
loolirer <loolirer@gmail.com>
2026-09-25 00:27:18 -0300

    move deterministic ssh setup to dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,19 +2,7 @@
 set -e
 
 # SSH Setup
-mkdir -p /run/sshd
-chmod 755 /run/sshd
 ls /etc/ssh/*_key >/dev/null 2>&1 || ssh-keygen -A
-
-sed -i -E 's/^#?Port .*/Port 8022/' /etc/ssh/sshd_config
-sed -i -E 's/^#?PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i -E 's/^#?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i -E 's/^#?PermitEmptyPasswords .*/PermitEmptyPasswords yes/' /etc/ssh/sshd_config
-sed -i -E 's/^#?UsePAM .*/UsePAM no/' /etc/ssh/sshd_config
-sed -i '/^AcceptEnv/d' /etc/ssh/sshd_config
-
-passwd -d root
-touch /root/.hushlogin
 
 # Uncomment for automatic operation
 # source /etc/profile.d/ros2.sh
